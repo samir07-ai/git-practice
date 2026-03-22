fad4c63430cd9fa226fc38c95e3910b2ec5deb69
Samir Acharya <samircsit25@oic.edu.np>
2026-03-22 11:01:00 +0545

    Updated file

--- file2.c
+++ file2.c
@@ -1,1 +1,21 @@
-file2 
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %llu\n", n, factorial);
+    }
+
+    return 0;
+}
+update 
